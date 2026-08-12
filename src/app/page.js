f281d2be0c861f4dfcbe9a21f6a3e19426c37418
amsein <shabanishabaniamirhossein98@gmail.com>
2026-08-12 19:54:08 +0330

    test page rendering

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,8 @@ import ScrollSections from "./_components/Scrollsection";
 import HeroAnimation from "./_components/HeroAnimation"; // ← جدید
 
 async function Page() {
+  console.log("🔥 HOME PAGE RENDER:", new Date().toISOString());
+
   const { landingHyperText, openTime, closeTime } = await getSetting();
   const categories = await getCategories();
   const suggestedItems = await getSuggestedItems();
